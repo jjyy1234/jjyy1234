b9dbf87da5c72514b0b7c2c1c79252db1c0ac90d
jjyy1234 <147390985+jjyy1234@users.noreply.github.com>
2025-11-26 16:56:27 +0800

    Add files via upload

---
+++ PUBGOffsets.hpp
@@ -0,0 +1,112 @@
+#ifndef PUBGOffsets_hpp
+#define PUBGOffsets_hpp
+
+#include <stdio.h>
+#include <string.h>
+
+#define kLineOfSightTo "0x7A8"
+#define kPersistentLevel "0x30"
+#define kActorList "0xA0"
+#define kNetDriver "0x38"
+#define kServerConnection "0x78"
+#define kPlayerController "0x98"
+#define klocalPlayerController "0x30"
+
+#define kPawn "0x460"
+#define kCharacter "0x470"
+#define kControlRotation "0x488"
+
+#define kMyTeam "0x8E8"
+
+#define kCameraCache "0x4D0"
+#define kViewTarget "0x1050"
+
+#define kPlayerCameraManager "0x4F0"
+#define kMyHUD "0x4E8"
+
+#define kLegacyFontSize "0x134"
+#define kCanvas "0x4A0"
+#define kSizeX "0x40"
+#define kSizeY "0x44"
+
+#define kHealth "0xDC8"
+#define kHealthMax "0xDCC"
+#define kbDead "0xDE4"
+#define kbIsGunADS "0x1081"
+#define kCurrentVehicle "0xE10"
+#define kPoseState "0x1738"
+#define kNearDeatchComponent "0x1AF0"
+#define kBreathMax "0x16C"
+
+#define kbHidden "0x88"
+#define kPlayerName "0x910"
+#define kNation "0x920"
+#define kTeamID "0x948"
+#define kbIsAI "0x9E0"
+#define kbIsMLAI "0x9E1"
+#define kMLAIPlayer "0x938"
+#define kMesh "0x4B8"
+#define kStaticMesh "0x8C8"
+#define kLastRenderTime "0x430"
+#define kVelocity "0x12c"
+
+#define kRelativeRotation "0x190"
+#define kRootComponent "0x1b0"
+#define kRelativeLocation "0x184"
+#define kRelativeScale3D "0x19c"
+#define kComponentVelocity "0x260"
+#define kRepMovement "0xb0"
+#define kNearDeathBreath "0x1B00"
+
+#define kbIsWeaponFiring "0x1728"
+
+#define kWeaponManagerComponent "0x2508"
+#define kCurrentWeaponReplicated "0x558"
+
+#define kShootWeaponComponent "0xF30"
+#define kShootWeaponEntityComp "0x1268"
+
+#define kWeaponId "0x178"
+
+#define kBulletFireSpeed "0x4E0"
+
+#define kRecoilKickADS "0xC70"
+
+#define kGameDeviationFactor "0xBAC"
+
+#define kShootMode "0x1090"
+
+#define kCurBullet "0xFB8"
+
+#define kCurMaxBullet "0xFD0"
+
+#define kVehicleCommon "0xBF8"
+
+#define kHP "0x33C"
+#define kHPMax "0x338"
+
+#define kFuel "0x41C"
+#define kFuelMax "0x418"
+
+#define kPickUpDataList "0x8B0"
+#define kGoodsID "0x38"
+#define kTableName "0x920"
+
+#define kCurrentStates "0xFB8"
+#define kFPS "0x1c4"
+
+#define kGameReplayType "0x944"
+#define kScopeFov "0x1a74"
+#define kPickUpAnim "0x1e28"
+
+#define kPressingFireBtn "0x33d0"
+
+#define kCurrentReloadWeapon "0x2b58"
+#define kCachedBulletTrackComponent "0xe28"
+#define wuhou "0x190"
+
+#define kYaw "0x888"
+#define kRoll "0x890"
+#define kPitch "0x880"
+
+#endif /* PUBGOffsets_hpp */
